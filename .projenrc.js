07b4431c79c534657bd6a17d3d503474947b4cf7
desiby <desiby@hotmail.com>
2021-10-20 13:39:38 -0400

    no spaces

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,4 @@
 const { JsiiProject } = require('projen');
-const mavenData = {
-  javaPackage: 'jar',
-  mavenArtifactId: 'jsii-java-module',
-  mavenGroupId: 'com.desiby',
-  mavenEndpoint: 'https://maven.pkg.github.com/',
-  mavenRepositoryUrl: 'https://maven.pkg.github.com/desiby/jsii-maven-module',
-  mavenServerId: 'github'
-};
 const project = new JsiiProject({
   author: 'desiby',
   authorAddress: 'desiby@hotmail.com',
@@ -16,11 +8,16 @@ const project = new JsiiProject({
   deps: ['projen'],
   description: "projen jsii module for maven to be reused in java projects",
   peerDeps: ['projen'],
-  // devDeps: [],
-  // packageName: undefined,
+  devDeps: [
+    'fs-extra',
+    '@types/fs-extra@^8',
+  ],
+  packageName: '@desiby/projen-java-demo',
   // release: undefined,
   dependabot: false, // Disabling because it is a demo project
   mergify: false, // Disabling because it is a demo project
-  publishToMaven: mavenData
+
+  npmDistTag: 'latest',
+  npmRegistryUrl: 'https://npm.pkg.github.com',
 });
 project.synth();
---
+++ src/JavaMicroserviceProject/index.ts
@@ -0,0 +1,75 @@
+import * as fs from 'fs-extra'; // eslint-disable-line
+import * as path from 'path'; // eslint-disable-line
+import { Component, SampleDir, TextFile } from 'projen';
+import { JavaProject, JavaProjectOptions } from 'projen/lib/java';
+import { pascalCase } from '../pascalCase';
+
+export interface JavaMicroserviceProjectOptions extends JavaProjectOptions {
+  /**
+   * Prettier ignore settings
+   * @default ['*']
+   */
+  readonly prettierIgnore?: string[];
+}
+
+/**
+ * CDK Day Demo Project in TypeScript
+ *
+ * @pjid cdkday-project
+ */
+export class JavaMicroserviceProject extends JavaProject {
+  public readonly projectName: string;
+
+  constructor(options: JavaMicroserviceProjectOptions) {
+    super({
+      ...options,
+      sample: true,
+      // Custom project stuff
+      artifactId: "jsii-java-module",
+      groupId: "com.idemia.isna.vss.microservice",
+      description: "Base Java Serverless Microservice template to be used for Idemia projects"
+
+    });
+
+    this.projectName = path.basename(process.cwd());
+
+    this.addDependency("org.postgresql/postgresql@42.2.20");
+
+    const prettierIgnoreLines = options.prettierIgnore ?? ['*'];
+    new TextFile(this, '.prettierignore', {
+      lines: prettierIgnoreLines,
+    });
+
+    new Sample(this);
+    // if (options.sample ?? true) {
+    //   new Sample(this);
+    // }
+  }
+  
+}
+
+class Sample extends Component {
+  private readonly demoProject: JavaMicroserviceProject;
+
+
+  constructor(project: JavaMicroserviceProject) {
+    super(project);
+    this.demoProject = project;
+  }
+
+  public synthesize() {
+    
+
+    //const projectType = pascalCase(this.demoProject.projectName);
+
+    new SampleDir(this.demoProject, ".", {
+      sourceDir: "gitlab_ci.yml"
+    });
+
+  }
+
+//   private createMainTsContents(projectName: string, projectType: string): string {
+//     return ``;
+//   }
+
+}
